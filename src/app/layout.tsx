51cb13668a6cac6d82a7b462571bc7dfaa131608
hi-rachel <rachel.uiux@gmail.com>
2025-05-20 22:48:28 +0900

    style: 글로벌 스타일 작성, 폰트 설정
    - layout 전역에 pretendard 설정시 다른 폰트 적용되지 않는 이슈로 모두 globals.css cdn으로 관리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,20 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import ClientLayout from "@/components/layout/ClientLayout";
-
-const pretendard = localFont({
-  src: "./fonts/PretendardVariable.woff2",
-  display: "swap",
-  weight: "45 920",
-  variable: "--font-pretendard",
-});
 
 export const metadata: Metadata = {
   title: "BitBetter",
   description: "1% Everyday Better",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
-    <html lang="en">
-      <body className={`${pretendard.variable} antialiased`}>
+    <html lang="en" className="scroll-smooth">
+      <body className="antialiased">
         <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
   );
-}
+};
+
+export default RootLayout;
