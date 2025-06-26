220b1898d337dbf202fe8f1695f8557ebe64483e
hi-rachel <rachel.uiux@gmail.com>
2025-06-26 17:04:47 +0900

    style: hr 스타일 조정

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -24,10 +24,11 @@ const BlogDetailPage = () => {
   return (
     <main className="container mx-auto px-4 py-8">
       <div className="max-w-3xl mx-auto">
-        <h1 className="text-4xl font-extrabold mb-4">{post.title}</h1>
+        <h1 className="text-5xl font-extrabold mb-4">{post.title}</h1>
         <p className="text-sm text-gray-500 mb-8">
           {format(new Date(post.date), "yyyy-MM-dd")}
         </p>
+        <hr />
         <article>
           <MDXRenderer code={post.body.code} />
         </article>
