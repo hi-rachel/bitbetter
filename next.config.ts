f41fdb6a24cf739ef8dba5949ef30ea91231e9ff
hi-rachel <rachel.uiux@gmail.com>
2025-08-26 20:50:15 +0900

    chore(next.config.ts): 이미지 도메인 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
+import type { NextConfig } from "next";
 import { withContentlayer } from "next-contentlayer";
 
-const nextConfig = {
+const nextConfig: NextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.youtube.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default withContentlayer(nextConfig);
